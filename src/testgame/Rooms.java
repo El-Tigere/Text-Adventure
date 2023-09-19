9f151d303e41dd25c2a4b02b7440460304fb2fb9
El_Tigere <eltigere@web.de>
2023-09-19 15:40:39 +0200

    added test interaction

--- src/testgame/Rooms.java
+++ src/testgame/Rooms.java
@@ -1,7 +1,11 @@
 package testgame;
 
+import engine.Interaction;
 import engine.Room;
 
 public class Rooms {
     public static final Room ENTRY_ROOM = new Room("You are in an empty room.");
+    static {
+        ENTRY_ROOM.addInteraction(new Interaction("abc"));
+    }
 }

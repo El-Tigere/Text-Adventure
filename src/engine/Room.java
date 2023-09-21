6b5fe0ebc1be96e6b219429c15f1855a67440569
El_Tigere <eltigere@web.de>
2023-09-21 19:42:06 +0200

    added Room class

---
+++ src/engine/Room.java
@@ -0,0 +1,13 @@
+package engine;
+
+public class Room {
+    private String description;
+    
+    public Room(String description) {
+        this.description = description;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+}

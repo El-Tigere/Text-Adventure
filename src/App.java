3a997b69745e6f07cd5df978a49dda09f160c3a8
El_Tigere <eltigere@web.de>
2023-09-23 13:29:17 +0200

    added game loop

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
+import engine.Game;
+import engine.Player;
+import testgame.Rooms;
+
 public class App {
     
     public static void main(String[] args) throws Exception {
+        Game game = new Game(Rooms.ENTRY_ROOM);
+        Player player =new Player(game);
+        player.printInfo(System.out);
         
+        Scanner inputScanner = new Scanner(System.in);
+        String input;
+        while(true) {
+            System.out.println("What do you want to do?");
+            input = inputScanner.nextLine();
+            if(input.equals("exit")) break;
+            player.executeCommand(input, System.out);
+        }
+        
+        inputScanner.close();
     }
 }

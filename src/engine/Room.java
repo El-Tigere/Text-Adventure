3ccea92036e1d8449a54a605a86d5fe6b076464b
El_Tigere <eltigere@web.de>
2023-09-23 13:49:13 +0200

    added interaction to Room class

--- src/engine/Room.java
+++ src/engine/Room.java
@@ -1,10 +1,18 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class Room {
     private String description;
+    private ArrayList<Interaction> interactions;
     
     public Room(String description) {
         this.description = description;
+        interactions = new ArrayList<Interaction>();
+    }
+    
+    public void addInteraction(Interaction interaction) {
+        interactions.add(interaction);
     }
     
     public String getDescription() {

513cb79edaed971f4963c2af9a621ed99c74d418
El_Tigere <eltigere@web.de>
2023-10-05 23:39:39 +0200

    added brick to test room

--- src/testgame/Rooms.java
+++ src/testgame/Rooms.java
@@ -1,11 +1,13 @@
 package testgame;
 
 import engine.Interaction;
+import engine.ItemInteraction;
 import engine.Room;
 
 public class Rooms {
     public static final Room ENTRY_ROOM = new Room("You are in an empty room.");
     static {
         ENTRY_ROOM.addInteraction(new Interaction("abc"));
+        ENTRY_ROOM.addInteraction(new ItemInteraction(Items.BRICK));
     }
 }

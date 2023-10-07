4f8bbdf61c1c1bf6cf98742b6df12f481479e843
El_Tigere <eltigere@web.de>
2023-10-07 16:21:43 +0200

    corrected spelling of brick

--- src/testgame/Items.java
+++ src/testgame/Items.java
@@ -3,5 +3,5 @@ package testgame;
 import engine.Item;
 
 public class Items {
-    public static final Item BRICK = new Item("Brick", "A red brick with an example-description.");
+    public static final Item BRICK = new Item("brick", "A red brick with an example-description.");
 }

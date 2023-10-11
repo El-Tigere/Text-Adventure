d4a868bced77b90cb4f7988918230193afecf7d5
El_Tigere <eltigere@web.de>
2023-10-11 15:14:23 +0200

    added second test room

--- src/testgame/Rooms.java
+++ src/testgame/Rooms.java
@@ -1,13 +1,19 @@
 package testgame;
 
 import engine.Room;
+import engine.interactions.DoorInteraction;
 import engine.interactions.Interaction;
 import engine.interactions.ItemInteraction;
 
 public class Rooms {
     public static final Room ENTRY_ROOM = new Room("You are in an empty room.");
+    public static final Room LIBRARY = new Room("You are in a large library with bookshelves that reach up to the cieling.");
+    
     static {
         ENTRY_ROOM.addInteraction(new Interaction("abc"));
         ENTRY_ROOM.addInteraction(new ItemInteraction(Items.BRICK));
+        ENTRY_ROOM.addInteraction(new DoorInteraction("wide wooden door", LIBRARY));
+        
+        LIBRARY.addInteraction(new DoorInteraction("wide wooden door", ENTRY_ROOM));
     }
 }

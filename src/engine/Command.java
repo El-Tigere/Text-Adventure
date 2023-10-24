1070235be15d983bb9a183f77b7604ada5c5e341
El_Tigere <eltigere@web.de>
2023-10-24 14:16:13 +0200

    added getter for COMMANDS

--- src/engine/Command.java
+++ src/engine/Command.java
@@ -103,7 +103,7 @@ public class Command {
         }),
         new Command("help", "get a list of all available commands", (Player player, String params, PrintStream stream) -> {
             stream.println("You can use the following commands:");
-            for(Command c : COMMANDS) {
+            for(Command c : getCommands()) {
                 stream.print(c.commandName);
                 stream.print(" - ");
                 stream.println(c.description);
@@ -119,6 +119,10 @@ public class Command {
         this.commandName = commandName;
         this.description = description;
         this.function = function;
+    }
+    
+    private static Command[] getCommands() {
+        return COMMANDS;
     }
     
     public static void executeString(Player player, String commandString, PrintStream stream) {

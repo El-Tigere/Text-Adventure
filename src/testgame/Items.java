2bf6dcd027a0516e5247ff2108c90a374ab7fa35
El_Tigere <eltigere@web.de>
2023-10-26 18:00:26 +0200

    added book item

--- src/testgame/Items.java
+++ src/testgame/Items.java
@@ -4,4 +4,5 @@ import engine.Item;
 
 public class Items {
     public static final Item BRICK = new Item("brick", "A red brick with an example-description.");
+    public static final Item BOOK = new Item("book", "An example-book given to you by carl the librarian.");
 }

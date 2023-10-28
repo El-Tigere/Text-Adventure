f09fa767ca9ca29b781c13f845eab98c8ac0dfb6
El_Tigere <eltigere@web.de>
2023-10-28 14:06:51 +0200

    added give reaction to npcs

--- src/engine/interactions/NPCInteraction.java
+++ src/engine/interactions/NPCInteraction.java
@@ -3,14 +3,17 @@ package engine.interactions;
 import java.io.PrintStream;
 
 import engine.Ansi;
+import engine.Item;
 import engine.Player;
 
 public class NPCInteraction extends Interaction {
     final INPCInteractionDialogue dialogue;
+    final INPCInteractionGive giveReaction;
     
-    public NPCInteraction(String name, INPCInteractionDialogue dialogue) {
+    public NPCInteraction(String name, INPCInteractionDialogue dialogue, INPCInteractionGive giveReaction) {
         super(name);
         this.dialogue = dialogue;
+        this.giveReaction = giveReaction;
     }
     
     @Override
@@ -31,8 +34,17 @@ public class NPCInteraction extends Interaction {
         dialogue.talk(player, stream);
     }
     
+    public boolean give(Player player, Item item, PrintStream stream) {
+        return giveReaction.give(player, item, stream);
+    }
+    
     @FunctionalInterface
     public interface INPCInteractionDialogue {
         public void talk(Player player, PrintStream stream);
+    }
+    
+    @FunctionalInterface
+    public interface INPCInteractionGive {
+        public boolean give(Player player, Item item, PrintStream stream);
     }
 }
